9f2e77f3f0dae46f5dc2322308b34359449f87c4
Gregor Tudan <gregor.tudan@cofinpro.de>
2017-07-12 10:09:44 +0200

    convert to argParse

--- check_idnow.py
+++ check_idnow.py
@@ -1,4 +1,4 @@
-! /usr/bin/env python
+#! /usr/bin/env python
 
 ###############################################################################
 # Nagios plugin template 
@@ -15,19 +15,14 @@
 __author__ = 'Your Name Here'
 __version__= 0.1
 
-from optparse import OptionParser, OptionGroup
+from argparse import ArgumentParser
 import logging as log
-
-## These will override any args passed to the script normally. Comment out after testing.
-#testargs = '--help'
-#testargs = '--version'
-#testargs = '-vvv'
 
 def main():
     """ Main plugin logic goes here """
 
     ## Parse command-line arguments
-    args, args2 = parse_args()
+    args = parse_args()
 
     ## Uncomment to test logging levels against verbosity settings
     # log.debug('debug message')
@@ -43,39 +38,33 @@ def main():
 def parse_args():
     """ Parse command-line arguments """
 
-    parser = OptionParser(usage='usage: %prog [-v|vv|vvv] [options]',
-                          version='{0}: v.{1} by {2}'.format('%prog', __version__, __author__))
+    parser = ArgumentParser(usage='usage: %prog [-v|vv|vvv] [options]',
+                            epilog='{0}: v.{1} by {2}'.format('%prog', __version__, __author__))
 
     ## Verbosity (want this first, so it's right after --help and --version)
-    parser.add_option('-v', help='Set verbosity level',
-                      action='count', default=0, dest='v')
+    parser.add_argument('-v', help='Set verbosity level',
+                        action='count', default=0, dest='v')
 
     ## CLI arguments specific to this script
-    group = OptionGroup(parser,'Plugin Options')
-    group.add_option('-x', '--extra', help='Your option here',
+    group = parser.add_argument_group(parser,'Plugin Options')
+    group.add_argument('-x', '--extra', help='Your option here',
                      default=None)
     
     ## Common CLI arguments
-    #parser.add_option('-c', '--critical', help='Set the critical threshold. Default: %(default)s',
-    #                  default=97, type=float, dest='crit', metavar='##')
-    #parser.add_option('-w', '--warning', help='Set the warning threshold. Default: %(default)s',
-    #                  default=95, type=float, dest='warn', metavar='##')
+    parser.add_argument('-c', '--critical', help='Set the critical threshold. Default: %(default)s',
+                      default=97, type=float, dest='crit', metavar='##')
+    parser.add_argument('-w', '--warning', help='Set the warning threshold. Default: %(default)s',
+                      default=95, type=float, dest='warn', metavar='##')
     
-    parser.add_option_group(group)
 
-    ## Try to parse based on the testargs variable. If it doesn't exist, use args
-    try:
-        args, args2 = parser.parse_args(testargs.split())
-    except NameError:
-        args, args2 = parser.parse_args()
+    args = parser.parse_args()
 
     ## Set the logging level based on the -v arg
     log.getLogger().setLevel([log.ERROR, log.WARN, log.INFO, log.DEBUG][args.v])
 
     log.debug('Parsed arguments: {0}'.format(args))
-    log.debug('Other  arguments: {0}'.format(args2))
 
-    return args, args2
+    return args
 
 def gtfo(exitcode, message=''):
     """ Exit gracefully with exitcode and (optional) message """
